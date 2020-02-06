5cf051cc60fe8e0551aa3b8c4a215715e892adcd
Landon Woerdeman <13247759+lwoerdeman@users.noreply.github.com>
2020-02-06 17:04:13 -0600

    Test alerts, setup static navbar

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,7 +12,11 @@ export class HomeComponent implements OnInit {
   constructor(private alertService: AlertService) { }
 
   ngOnInit() {
-    setTimeout(() => this.alertService.sendAlert(new AlertMessage("This is information", AlertType.INFO)), 2000)
+    setTimeout(() => this.alertService.sendAlert(new AlertMessage("This is information", AlertType.INFO)), 2000);
+    setTimeout(() => this.alertService.sendAlert(new AlertMessage("This is a warning", AlertType.WARNING)), 3000);
+    setTimeout(() => this.alertService.sendAlert(new AlertMessage("This is an error", AlertType.ERROR)), 4000);
+    setTimeout(() => this.alertService.sendAlert(new AlertMessage("This is a success", AlertType.SUCCESS)), 5000);
+
   }
 
 }
