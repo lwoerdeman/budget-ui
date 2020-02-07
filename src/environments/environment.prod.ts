6a76fa693a611cc7da58c60e09ce5881d0bd324f
Landon Woerdeman <13247759+lwoerdeman@users.noreply.github.com>
2020-02-06 22:36:57 -0600

    Save firebase config to prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDWVOzWn3Gh66Y0BQHAQOn2FT06iZbRW4M",
+  authDomain: "budget-bb5a0.firebaseapp.com",
+  databaseURL: "https://budget-bb5a0.firebaseio.com",
+  projectId: "budget-bb5a0",
+  storageBucket: "budget-bb5a0.appspot.com",
+  messagingSenderId: "884655347316",
+  appId: "1:884655347316:web:e5df2d849c29f093"
+};
+
 export const environment = {
-  production: true
+  production: true,
+  firebase: firebaseConfig
 };
